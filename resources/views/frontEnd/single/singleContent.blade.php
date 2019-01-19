6f6e4f0177cf72f17ad1ee39db734e88fcbdbab6
ridwanhoque <ridwanhoque2@gmail.com>
2019-01-19 22:57:41 +0600

    product details in frontend

--- resources/views/frontEnd/single/singleContent.blade.php
+++ resources/views/frontEnd/single/singleContent.blade.php
@@ -26,29 +26,14 @@ Single
 						</div>
 					</div>
 					<div class="col-md-6 single-top-right">
-						<h3 class="item_name">Electric Snow Blower</h3>
-						<p>Processing Time: Item will be shipped out within 2-3 working days. </p>
-						<div class="single-rating">
-							<ul>
-								<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
-								<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
-								<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
-								<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
-								<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
-								<li class="rating">20 reviews</li>
-								<li><a href="#">Add your review</a></li>
-							</ul> 
-						</div>
+						<h3 class="item_name">{{ $singleProduct->product_name }}</h3>
+						<p>{!! $singleProduct->product_short_description !!}</p>
 						<div class="single-price">
 							<ul>
-								<li>$540</li>  
-								<li><del>$600</del></li> 
-								<li><span class="w3off">10% OFF</span></li> 
-								<li>Ends on: June,5th</li>
-								<li><a href="#"><i class="fa fa-gift" aria-hidden="true"></i> Coupon</a></li>
+								<li>BDT {{ $singleProduct->product_price }}</li>  
 							</ul>	
 						</div> 
-						<p class="single-price-text">Fusce a egestas nibh, eget ornare erat. Proin placerat, urna et consequat efficitur, sem odio blandit enim, sit amet euismod turpis est mattis lectus. Vestibulum maximus quam et quam egestas imperdiet. In dignissim auctor viverra. </p>
+						<p class="single-price-text">{!! $singleProduct->product_long_description !!}</p>
 						<form action="#" method="post">
 							<input type="hidden" name="cmd" value="_cart" />
 							<input type="hidden" name="add" value="1" /> 
@@ -60,180 +45,7 @@ Single
 					</div>
 				   <div class="clearfix"> </div>  
 				</div>
-				<div class="single-page-icons social-icons"> 
-					<ul>
-						<li><h4>Share on</h4></li>
-						<li><a href="#" class="fa fa-facebook icon facebook"> </a></li>
-						<li><a href="#" class="fa fa-twitter icon twitter"> </a></li>
-						<li><a href="#" class="fa fa-google-plus icon googleplus"> </a></li>
-						<li><a href="#" class="fa fa-dribbble icon dribbble"> </a></li>
-						<li><a href="#" class="fa fa-rss icon rss"> </a></li> 
-					</ul>
-				</div>
 			</div> 
-			<!-- recommendations -->
-			<div class="recommend">
-				<h3 class="w3ls-title">Our Recommendations </h3> 
-				<script>
-					$(document).ready(function() { 
-						$("#owl-demo5").owlCarousel({
-					 
-						  autoPlay: 3000, //Set AutoPlay to 3 seconds
-					 
-						  items :4,
-						  itemsDesktop : [640,5],
-						  itemsDesktopSmall : [414,4],
-						  navigation : true
-					 
-						});
-						
-					}); 
-				</script>
-				<div id="owl-demo5" class="owl-carousel">
-					<div class="item">
-						<div class="glry-w3agile-grids agileits">
-							<div class="new-tag"><h6>20% <br> Off</h6></div>
-							<a href="products1.html"><img src="{{ asset("public/frontEnd/images/f2.png") }}" alt="img"></a>
-							<div class="view-caption agileits-w3layouts">           
-								<h4><a href="products1.html">Women Sandal</a></h4>
-								<p>Lorem ipsum dolor sit amet consectetur</p>
-								<h5>$20</h5>
-								<form action="#" method="post">
-									<input type="hidden" name="cmd" value="_cart" />
-									<input type="hidden" name="add" value="1" /> 
-									<input type="hidden" name="w3ls_item" value="Women Sandal" /> 
-									<input type="hidden" name="amount" value="20.00" /> 
-									<button type="submit" class="w3ls-cart" ><i class="fa fa-cart-plus" aria-hidden="true"></i> Add to cart</button>
-								</form>
-							</div>        
-						</div> 
-					</div>
-					<div class="item">
-						<div class="glry-w3agile-grids agileits"> 
-							<a href="products.html"><img src="{{ asset("public/frontEnd/images/e4.png") }}" alt="img"></a>
-							<div class="view-caption agileits-w3layouts">           
-								<h4><a href="products.html">Digital Camera</a></h4>
-								<p>Lorem ipsum dolor sit amet consectetur</p>
-								<h5>$80</h5>
-								<form action="#" method="post">
-									<input type="hidden" name="cmd" value="_cart" />
-									<input type="hidden" name="add" value="1" /> 
-									<input type="hidden" name="w3ls_item" value="Digital Camera"/> 
-									<input type="hidden" name="amount" value="100.00" /> 
-									<button type="submit" class="w3ls-cart" ><i class="fa fa-cart-plus" aria-hidden="true"></i> Add to cart</button>
-								</form>
-							</div>         
-						</div>  
-					</div>  
-					<div class="item">
-						<div class="glry-w3agile-grids agileits"> 
-							<div class="new-tag"><h6>New</h6></div>
-							<a href="products4.html"><img src="{{ asset("public/frontEnd/images/s1.png") }}" alt="img"></a>
-							<div class="view-caption agileits-w3layouts">           
-								<h4><a href="products4.html">Roller Skates</a></h4>
-								<p>Lorem ipsum dolor sit amet consectetur</p>
-								<h5>$180</h5>
-								<form action="#" method="post">
-									<input type="hidden" name="cmd" value="_cart" />
-									<input type="hidden" name="add" value="1" /> 
-									<input type="hidden" name="w3ls_item" value="Roller Skates"/> 
-									<input type="hidden" name="amount" value="180.00" /> 
-									<button type="submit" class="w3ls-cart" ><i class="fa fa-cart-plus" aria-hidden="true"></i> Add to cart</button>
-								</form>
-							</div>         
-						</div>  
-					</div>
-					<div class="item">
-						<div class="glry-w3agile-grids agileits"> 
-							<a href="products1.html"><img src="{{ asset("public/frontEnd/images/f1.png") }}" alt="img"></a>
-							<div class="view-caption agileits-w3layouts">           
-								<h4><a href="products1.html">T Shirt</a></h4>
-								<p>Lorem ipsum dolor sit amet consectetur</p>
-								<h5>$10</h5>
-								<form action="#" method="post">
-									<input type="hidden" name="cmd" value="_cart" />
-									<input type="hidden" name="add" value="1" /> 
-									<input type="hidden" name="w3ls_item" value="T Shirt" /> 
-									<input type="hidden" name="amount" value="10.00" /> 
-									<button type="submit" class="w3ls-cart" ><i class="fa fa-cart-plus" aria-hidden="true"></i> Add to cart</button>
-								</form>
-							</div>        
-						</div>    
-					</div>
-					<div class="item">
-						<div class="glry-w3agile-grids agileits"> 
-							<div class="new-tag"><h6>New</h6></div>
-							<a href="products6.html"><img src="{{ asset("public/frontEnd/images/p1.png") }}" alt="img"></a>
-							<div class="view-caption agileits-w3layouts">           
-								<h4><a href="products6.html">Coffee Mug</a></h4>
-								<p>Lorem ipsum dolor sit amet consectetur</p>
-								<h5>$14</h5>
-								<form action="#" method="post">
-									<input type="hidden" name="cmd" value="_cart" />
-									<input type="hidden" name="add" value="1" /> 
-									<input type="hidden" name="w3ls_item" value="Coffee Mug" /> 
-									<input type="hidden" name="amount" value="14.00" /> 
-									<button type="submit" class="w3ls-cart" ><i class="fa fa-cart-plus" aria-hidden="true"></i> Add to cart</button>
-								</form>
-							</div>         
-						</div>  
-					</div>
-					<div class="item">
-						<div class="glry-w3agile-grids agileits"> 
-							<div class="new-tag"><h6>20% <br> Off</h6></div>
-							<a href="products6.html"><img src="{{ asset("public/frontEnd/images/p2.png") }}" alt="img"></a>
-							<div class="view-caption agileits-w3layouts">           
-								<h4><a href="products6.html">Teddy bear</a></h4>
-								<p>Lorem ipsum dolor sit amet consectetur</p>
-								<h5>$20</h5>
-								<form action="#" method="post">
-									<input type="hidden" name="cmd" value="_cart" />
-									<input type="hidden" name="add" value="1" /> 
-									<input type="hidden" name="w3ls_item" value="Teddy bear" /> 
-									<input type="hidden" name="amount" value="20.00" /> 
-									<button type="submit" class="w3ls-cart" ><i class="fa fa-cart-plus" aria-hidden="true"></i> Add to cart</button>
-								</form>
-							</div>        
-						</div> 
-					</div>
-					<div class="item">
-						<div class="glry-w3agile-grids agileits"> 
-							<a href="products4.html"><img src="{{ asset("public/frontEnd/images/s2.png") }}" alt="img"></a>
-							<div class="view-caption agileits-w3layouts">           
-								<h4><a href="products4.html">Football</a></h4>
-								<p>Lorem ipsum dolor sit amet consectetur</p>
-								<h5>$70</h5>
-								<form action="#" method="post">
-									<input type="hidden" name="cmd" value="_cart" />
-									<input type="hidden" name="add" value="1" /> 
-									<input type="hidden" name="w3ls_item" value="Football"/> 
-									<input type="hidden" name="amount" value="70.00"/>
-									<button type="submit" class="w3ls-cart" ><i class="fa fa-cart-plus" aria-hidden="true"></i> Add to cart</button>
-								</form>
-							</div>        
-						</div> 
-					</div> 
-					<div class="item">
-						<div class="glry-w3agile-grids agileits"> 
-							<div class="new-tag"><h6>Sale</h6></div>
-							<a href="products3.html"><img src="{{ asset("public/frontEnd/images/h1.png") }}" alt="img"></a>
-							<div class="view-caption agileits-w3layouts">           
-								<h4><a href="products3.html">Wall Clock</a></h4>
-								<p>Lorem ipsum dolor sit amet consectetur</p>
-								<h5>$80</h5>
-								<form action="#" method="post">
-									<input type="hidden" name="cmd" value="_cart" />
-									<input type="hidden" name="add" value="1" /> 
-									<input type="hidden" name="w3ls_item" value="Wall Clock" /> 
-									<input type="hidden" name="amount" value="80.00" /> 
-									<button type="submit" class="w3ls-cart" ><i class="fa fa-cart-plus" aria-hidden="true"></i> Add to cart</button>
-								</form>
-							</div>         
-						</div>  
-					</div> 
-				</div>    
-			</div>
-			<!-- //recommendations --> 
 			<!-- collapse-tabs -->
 			<div class="collpse tabs">
 				<h3 class="w3ls-title">About this item</h3> 
@@ -248,7 +60,7 @@ Single
 						</div>
 						<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
 							<div class="panel-body">
-								Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+								{!! $singleProduct->product_short_description !!}
 							</div>
 						</div>
 					</div>
@@ -262,35 +74,7 @@ Single
 						</div>
 						<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
 							<div class="panel-body">
-								Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-							</div>
-						</div>
-					</div>
-					<div class="panel panel-default">
-						<div class="panel-heading" role="tab" id="headingThree">
-							<h4 class="panel-title">
-								<a class="collapsed pa_italic" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-									<i class="fa fa-check-square-o fa-icon" aria-hidden="true"></i> reviews (5) <span class="fa fa-angle-down fa-arrow" aria-hidden="true"></span> <i class="fa fa-angle-up fa-arrow" aria-hidden="true"></i>
-								</a>
-							</h4>
-						</div>
-						<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-							<div class="panel-body">
-								Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-							</div>
-						</div>
-					</div>
-					<div class="panel panel-default">
-						<div class="panel-heading" role="tab" id="headingFour">
-							<h4 class="panel-title">
-								<a class="collapsed pa_italic" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-									<i class="fa fa-question-circle fa-icon" aria-hidden="true"></i> help <span class="fa fa-angle-down fa-arrow" aria-hidden="true"></span> <i class="fa fa-angle-up fa-arrow" aria-hidden="true"></i>
-								</a>
-							</h4>
-						</div>
-						<div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
-							<div class="panel-body">
-								Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+								{!! $singleProduct->product_long_description !!}
 							</div>
 						</div>
 					</div>
